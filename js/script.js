ad60782ed3f744190543f469b1c7a99a4c81aeaf
AriSyafri <diamondThink15@gmail.com>
2024-04-10 01:25:43 +0700

    menambah fitur search hingga responsive search

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// toggle class active
+// toggle class active hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika hamburger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
@@ -6,12 +6,41 @@ document.querySelector('#hamburger-menu').onclick = () => {
 
 };
 
-// klik diluar sidebar untuk menghilangkan nav
+// klik diluar sidebar untuk menghilangkan nav hamburger menu
 const hamburger = document.querySelector('#hamburger-menu');
 
 document.addEventListener('click', function(e){
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+
+});
+
+
+// toggle class active search menu
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+
+// ketika search menu di klik
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+
+};
+
+// klik diluar elemen untuk menghilangkan nav search menu
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+
+document.addEventListener('click', function(e){
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 
 });
