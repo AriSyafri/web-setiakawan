40cf90ede2e40e0f95b79a0f78d1c7d270925d89
AriSyafri <diamondThink15@gmail.com>
2024-04-12 00:28:19 +0700

    menambah modal untuk produk

--- js/script.js
+++ js/script.js
@@ -62,3 +62,29 @@ document.addEventListener('click', function(e){
     }
 
 });
+
+// modal box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+});
+
+
+
+// klik tombol close modalbox
+const closeIcon = document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+};
